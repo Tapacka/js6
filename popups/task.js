5d56c06277f6190e4f06660887ca6881599c5ed1
Tapacka <88617836+Tapacka@users.noreply.github.com>
2022-07-08 19:12:50 +0300

    Update task.js
    11

--- popups/task.js
+++ popups/task.js
@@ -3,15 +3,16 @@ main.className = `${main.className} modal_active`;
 const modal_success = document.getElementById('modal_success');
 const closer = document.getElementsByClassName('modal__close');
 let array = Array.from(closer);
-array.forEach((p) => {
-    p.onclick = () => {    
+array.forEach((elem) => {
+    elem.onclick = () => {    
         main.className = 'modal';
         modal_success.className = 'modal';
 }})
 const success = document.getElementsByClassName('show-success');
 let array1 = Array.from(success);
 
-array1.forEach((p) => {
-    p.onclick = () => {
+array1.forEach((elem) => {
+    elem.onclick = () => {
+        main.className = 'modal'
         modal_success.className = 'modal modal_active';
 }})
