ee4ad8a9bf02ba78cf59929c6a3234249e8f5bcf
Tapacka <88617836+Tapacka@users.noreply.github.com>
2022-07-08 19:13:30 +0300

    Update task.js
    11

--- slider/task.js
+++ slider/task.js
@@ -1,32 +1,33 @@
-const arrow_prev = document.getElementsByClassName('slider__arrow_prev')
-const arrow_next = document.getElementsByClassName('slider__arrow_next')
-const slider_dots = document.getElementsByClassName('slider__dot')
+const arrowPrev = document.getElementsByClassName('slider__arrow_prev')
+const arrowNext = document.getElementsByClassName('slider__arrow_next')
+const sliderDots = document.getElementsByClassName('slider__dot')
 const items = document.getElementsByClassName('slider__item')
-items_array = Array.from(items)
-let length = items_array.length;
+let itemsArray = Array.from(items)
+let length = itemsArray.length;
 let index = 0;
-arrow_prev[0].onclick = () => {
-    Array.from(slider_dots).forEach((dot) => {dot.className = 'slider__dot'});    
-    items_array.forEach((p) => {p.className = 'slider__item'});
-    index = index - 1;
-    if (index<0) {index = length - 1};
-    items[index].className = 'slider__item slider__item_active';
-    Array.from(slider_dots)[index].className = 'slider__dot slider__dot_active';
+
+function makeActive(ind) {
+    Array.from(sliderDots).forEach((dot) => {dot.className = 'slider__dot'});
+    itemsArray.forEach((item) => {item.className = 'slider__item'});    
+    items[ind].className = 'slider__item slider__item_active';
+    Array.from(sliderDots)[ind].className = 'slider__dot slider__dot_active';
 }
-arrow_next[0].onclick = () => {
-    Array.from(slider_dots).forEach((dot) => {dot.className = 'slider__dot'});    
-    items_array.forEach((p) => {p.className = 'slider__item'});
+
+arrowPrev[0].onclick = () => {
+    index = index - 1
+    if (index<0) {index = length - 1};
+    makeActive(index)
+}
+
+arrowNext[0].onclick = () => {    
     index = index + 1;
     if (index>=length) {index = 0};
-    items[index].className = 'slider__item slider__item_active';
-    Array.from(slider_dots)[index].className = 'slider__dot slider__dot_active';
+    makeActive(index);  
 }
-Array.from(slider_dots).forEach((item, i) => {
-    item.onclick = () => {
-        Array.from(slider_dots).forEach((dot) => {dot.className = 'slider__dot'});
-        Array.from(slider_dots)[i].className = 'slider__dot slider__dot_active';
+
+Array.from(sliderDots).forEach((item, i) => {
+    item.onclick = () => {     
         index = i;
-        items_array.forEach((p) => {p.className = 'slider__item'});
-        items[i].className = 'slider__item slider__item_active';
+        makeActive(index);     
     }
 })
