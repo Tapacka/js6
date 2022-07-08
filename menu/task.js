e289710deac62c1dfa41e3f241a12a2e56188009
Tapacka <88617836+Tapacka@users.noreply.github.com>
2022-07-08 19:09:44 +0300

    Update task.js
    1

--- menu/task.js
+++ menu/task.js
@@ -1,13 +1,17 @@
 const menu = document.getElementsByClassName('menu_sub');
 let array1 = Array.from(menu);
-array1.forEach((p) => {
-    const parent = p.closest('.menu__item');    
+array1.forEach((item) => {
+    const parent = item.closest('.menu__item');    
     elem = parent.querySelector('.menu__link');
     elem.onclick = () => {
-        if(p.className == 'menu menu_sub') {
-        p.className = 'menu menu_sub menu_active';        
-        } else {p.className = 'menu menu_sub'};
-        array1.forEach((p1) => {if (p1!=p){p1.className='menu menu_sub'}});
+        if (item.className == 'menu menu_sub') {
+            item.className = 'menu menu_sub menu_active';        
+        } else {
+            item.className = 'menu menu_sub';
+        }
+        array1.forEach((item1) => {if (item1 != item) {
+            item1.className='menu menu_sub';
+        }})
         return false;
     }      
 })
